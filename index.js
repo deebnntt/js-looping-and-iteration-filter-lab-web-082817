6f54b1a020bd18c64eed9c482e9578c098ca881a
deebnntt <daniellebennettchan@gmail.com>
2017-10-09 13:19:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching (drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+    });
+  }
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter( function (name) {
+      return name.startsWith(string);
+  })
+}
+
+function matchName (drivers, string) {
+  return drivers.filter( function (key) {
+    return key['name'] === string;
+  })
+}
